fa8152819539b981ba291794ce6a873aef279d74
Kevin Cornelius <kfc328@gmail.com>
2015-07-29 00:05:04 +0800

    Just added a few more methods.
    Signed-off-by: Kevin Cornelius <kfc328@gmail.com>

--- Checkers/src/Hello.java
+++ Checkers/src/Hello.java
@@ -1,12 +1,18 @@
 
 public class Hello {
 	int x;
+	int y;
 	
-	Hello (int x) {
+	Hello (int x, int y) {
 		this.x = x;
+		this.y = y;
 	}
 	
 	int getX() {
 		return x;
+	}
+	
+	int getY() {
+		return y;
 	}
 }
